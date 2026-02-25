2387db5a2c132e97025701342788fe258f8c5a73
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-02-25 07:21:36 +0100

    refactor: share code between chrome and firefox
    - Move shared content.js, popup.js, popup.html to src/
    - Add browserAPI adapter for chrome/browser namespace
    - Add nix packages for chrome and firefox builds
    - Default package is now firefox
    - Remove build artifacts from repo (built via nix)

--- flake.nix
+++ flake.nix
@@ -6,8 +6,31 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      
+      mkExtension = name: outputFile: pkgs.stdenvNoCC.mkDerivation {
+        inherit name;
+        src = ./.;
+        
+        nativeBuildInputs = [ pkgs.zip ];
+        
+        buildPhase = ''
+          export HOME=$TMPDIR
+          bash pack.sh
+        '';
+        
+        installPhase = ''
+          mkdir -p $out
+          cp ${outputFile} $out/
+        '';
+      };
     in
       {
+        packages = {
+          chrome = mkExtension "nykredit-extension-chrome" "chrome.zip";
+          firefox = mkExtension "nykredit-extension-firefox" "firefox.xpi";
+          default = self.packages.${system}.firefox;
+        };
+        
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             zip
@@ -15,5 +38,4 @@
         };
       }
   );
-
 }
